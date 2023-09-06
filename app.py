db1896c6cdab973f68fcb2ac2e17ac11a1012bc2
PythonMarik <134636875+PythonMarik@users.noreply.github.com>
2023-09-06 21:11:08 +0300

    added dynamic data and API route

--- app.py
+++ app.py
@@ -1,10 +1,42 @@
-from flask import Flask
+from flask import Flask, render_template, jsonify
 
 app = Flask(__name__)
 
+BOSSES = [
+    {
+        'id': 1,
+        'Title': 'Owl',
+        'Location': 'Hirata',
+        'Difficulty': 'Hard'
+    },
+    {
+        'id': 2,
+        'Title': 'Geni',
+        'Location': 'Ashina Castle',
+        'Difficulty': 'Mid'
+    },
+    {
+        'id': 3,
+        'Title': 'Ishhin',
+        'Location': 'Haunted Place',
+        'Difficulty': 'Very Hard'
+    },
+    {
+        'id': 4,
+        'Title': 'Monk',
+        'Location': 'Deep Ashina',
+        'Difficulty': 'Low-Mid'
+    }
+]
+
 @app.route("/")
 def hello_world():
-    return "Hello, Marat"
+    return render_template('home.html', bosses=BOSSES)
+
+@app.route("/api/bosses")
+def list_bosses():
+    return jsonify(BOSSES)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
+
